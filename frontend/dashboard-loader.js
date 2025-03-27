9213b59dafb9c5533e181807cdbc7c8efbdeefd8
H01ST <150019043+H01ST@users.noreply.github.com>
2025-03-27 16:25:59 +0100

    Wow test data virker nu hvor oliver ikke amirmir mere

--- frontend/dashboard-loader.js
+++ frontend/dashboard-loader.js
@@ -12,11 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const dashboards = [
       {
         title: "Temperature",
-        url: "https://h01st.grafana.net/d-solo/c1385932-e8b5-481e-a999-f716359f8cf2/overview-celsius?orgId=1&from=1742391372458&to=1742996172458&timezone=utc&var-DS_PROMETHEUS=grafanacloud-demoinfra-prom&var-DS_LOKI=grafanacloud-demoinfra-logs&var-location=London&var-forecast_day=$__all&panelId=3&__feature.dashboardSceneSolo"
+        url: "https://h01st.grafana.net/d-solo/feh44r4tdzgn4e/temperature?orgId=1&from=1743088363400&to=1743088411599&timezone=browser&panelId=1&__feature.dashboardSceneSolo"
       },
       {
         title: "Humidity",
-        url: "https://h01st.grafana.net/d-solo/c1385932-e8b5-481e-a999-f716359f8cf2/overview-celsius?orgId=1&from=1742391372458&to=1742996172458&timezone=utc&var-DS_PROMETHEUS=grafanacloud-demoinfra-prom&var-DS_LOKI=grafanacloud-demoinfra-logs&var-location=London&var-forecast_day=$__all&panelId=5&__feature.dashboardSceneSolo"
+        url: "https://h01st.grafana.net/d-solo/eeh44w2ugyjnkd/humidity?orgId=1&from=1743088428826&to=1743088464831&timezone=browser&panelId=2&__feature.dashboardSceneSolo"
       }
     ];
     
